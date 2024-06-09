1be58b7358b70b7ae54403ed060ac890211ed022
AleexxG <alexanderg.sln@gmail.com>
2024-06-09 22:46:08 +0200

    Listing configuration for category and condition

---
+++ config/listing.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'condition' => [
+        'old',
+        'used',
+        'new',
+    ],
+
+    'category' => [
+        "Electronics",
+        "Fashion",
+        "Home & Garden",
+        "Motors",
+        "Collectibles & Art",
+        "Sporting Goods",
+        "Toys & Hobbies",
+        "Business & Industrial",
+        "Health & Beauty",
+        "Music",
+        "Books",
+        "Movies & TV",
+        "Video Games & Consoles",
+        "Cameras & Photo",
+        "Computers/Tablets & Networking",
+        "Cell Phones & Accessories",
+        "Jewelry & Watches",
+        "Crafts",
+        "Pet Supplies",
+        "Baby Essentials",
+    ],
+];
