0efec6d9aa20f16d4f33fd34db2ed4ca97ca4971
AleexxG <alexanderg.sln@gmail.com>
2024-06-14 20:27:58 +0200

    Validation added for create listing form

--- app/Livewire/CreateListing.php
+++ app/Livewire/CreateListing.php
@@ -8,13 +8,31 @@ use Livewire\Component;
 class CreateListing extends Component
 {
     public string $title = '';
-    public ?float $price = null;
+    public string $price = '';
     public string $condition = '';
     public string $category = '';
     public string $description = '';
 
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|min:2|max:255',
+            'price' => 'required|numeric|min:0.1',
+            'condition' => 'required|in:' . implode(',', config('listing.condition')),
+            'category' => 'required|in:' . implode(',', config('listing.category')),
+            'description' => 'required|string|min:10|max:1000',
+        ];
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
     public function createListing(): void
     {
+        $this->validate();
+
         Listing::create(array_merge(
             ['user_id' => auth()->id()],
             $this->only(['title', 'price', 'condition', 'category', 'description'])
