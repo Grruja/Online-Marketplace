ad38b3fb054c673fc3fe2da7e25b778925e3b680
AleexxG <alexanderg.sln@gmail.com>
2024-06-14 19:08:49 +0200

    Create listing method added

--- app/Livewire/CreateListing.php
+++ app/Livewire/CreateListing.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\Listing;
 use Livewire\Component;
 
 class CreateListing extends Component
 {
+    public string $title = '';
+    public ?float $price = null;
+    public string $condition = '';
+    public string $category = '';
+    public string $description = '';
+
+    public function createListing(): void
+    {
+        Listing::create(array_merge(
+            ['user_id' => auth()->id()],
+            $this->only(['title', 'price', 'condition', 'category', 'description'])
+        ));
+
+        $this->reset();
+        $this->redirectRoute('dashboard', navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.create-listing');
