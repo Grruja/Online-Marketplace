efbec41c662958fc4bea72aff8cea7cdede0d461
AleexxG <alexanderg.sln@gmail.com>
2024-06-14 19:08:23 +0200

    Added livewire to the form with error messages

--- resources/views/livewire/create-listing.blade.php
+++ resources/views/livewire/create-listing.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -7,36 +7,41 @@
                 </h1>
             </div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 mt-10">
-                <form>
+                <form wire:submit="createListing">
                     <div>
                         <x-input-label for="title" :value="__('Title')" />
-                        <x-text-input id="title" class="block mt-1 w-full" />
+                        <x-text-input wire:model="title" id="title" class="block mt-1 w-full" />
+                        @error('title') <em class="text-red-600 text-sm">{{ $message }}</em> @enderror
                     </div>
                     <div class="mt-5">
-                        <x-input-label for="description" :value="__('Description')" />
-                        <textarea id="description" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full"></textarea>
+                        <x-input-label for="price" :value="__('Price')" />
+                        <input wire:model="price" id="price" type="number" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full">
+                        @error('price') <em class="text-red-600 text-sm">{{ $message }}</em> @enderror
                     </div>
                     <div class="mt-5">
                         <x-input-label for="condition" :value="__('Condition')" />
-                        <select id="condition" class="cursor-pointer border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full">
+                        <select wire:model="condition" id="condition" class="cursor-pointer border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full">
                             <option disabled selected value=""></option>
                             @foreach(config('listing.condition') as $condition)
                                 <option value="{{ $condition }}">{{ ucwords($condition) }}</option>
                             @endforeach
                         </select>
-                    </div>
-                    <div class="mt-5">
-                        <x-input-label for="price" :value="__('Price')" />
-                        <input id="price" type="number" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full">
+                        @error('condition') <em class="text-red-600 text-sm">{{ $message }}</em> @enderror
                     </div>
                     <div class="mt-5">
                         <x-input-label for="category" :value="__('Category')" />
-                        <select id="category" class="cursor-pointer border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full">
+                        <select wire:model="category" id="category" class="cursor-pointer border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full">
                             <option disabled selected value=""></option>
                             @foreach(config('listing.category') as $category)
                                 <option value="{{ $category }}">{{ ucwords($category) }}</option>
                             @endforeach
                         </select>
+                        @error('category') <em class="text-red-600 text-sm">{{ $message }}</em> @enderror
+                    </div>
+                    <div class="mt-5">
+                        <x-input-label for="description" :value="__('Description')" />
+                        <textarea wire:model="description" id="description" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full" rows="7"></textarea>
+                        @error('description') <em class="text-red-600 text-sm">{{ $message }}</em> @enderror
                     </div>
 
                     <x-primary-button wire:loading.class="opacity-70" class="mt-10">
@@ -46,4 +51,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
