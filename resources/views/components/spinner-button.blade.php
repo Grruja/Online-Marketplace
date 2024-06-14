994c0f398a0c8724f8f8963b560a75aa5baa839a
AleexxG <alexanderg.sln@gmail.com>
2024-06-14 20:27:17 +0200

    Added wire target to spinner

--- resources/views/components/spinner-button.blade.php
+++ resources/views/components/spinner-button.blade.php
@@ -1,6 +1,8 @@
+@props(['target'])
+
 <div>
-    <span wire:loading.remove>{{ $slot }}</span>
-    <div wire:loading>
+    <span wire:loading.remove wire:target="{{ $target }}">{{ $slot }}</span>
+    <div wire:loading wire:target="{{ $target }}">
         <span class="inline-block animate-spin text-inherit size-3 me-1 border-[2px] border-current border-t-transparent rounded-full" role="status" aria-label="loading"></span>
         <span>{{ $slot }}</span>
     </div>
