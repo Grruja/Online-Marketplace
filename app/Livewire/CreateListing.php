4880cde5a2b26583d72b8e1690c036faebacf31b
AleexxG <alexanderg.sln@gmail.com>
2024-06-17 00:00:42 +0200

    Changed image rule for max size

--- app/Livewire/CreateListing.php
+++ app/Livewire/CreateListing.php
@@ -27,7 +27,7 @@ class CreateListing extends Component
             'price' => 'required|numeric|min:0.1',
             'condition' => 'required|in:'.implode(',', config('listing.condition')),
             'category' => 'required|in:'.implode(',', config('listing.category')),
-            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:5120',
             'description' => 'required|string|min:10|max:1000',
         ];
     }
