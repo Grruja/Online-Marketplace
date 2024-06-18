121b24a3b70f1c98df6ed3cde19a38dc22e0624e
AleexxG <alexanderg.sln@gmail.com>
2024-06-18 15:48:40 +0200

    Autocomplete off

--- resources/views/components/search-input.blade.php
+++ resources/views/components/search-input.blade.php
@@ -6,7 +6,7 @@
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="default-search" class="w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Search for anything" />
+        <input type="search" autocomplete="off" id="default-search" class="w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Search for anything" />
         <x-primary-button class="absolute end-1.5 bottom-1.5">Search</x-primary-button>
     </div>
 </div>
