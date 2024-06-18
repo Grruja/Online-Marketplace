c6ec1ea861ed466951649631b49322de6d2b2dd5
AleexxG <alexanderg.sln@gmail.com>
2024-06-18 15:40:43 +0200

    Updated listing category array

--- config/listing.php
+++ config/listing.php
@@ -8,25 +8,98 @@ return [
     ],
 
     'category' => [
-        "electronics",
-        "fashion",
-        "home & garden",
-        "motors",
-        "collectibles & art",
-        "sporting goods",
-        "toys & hobbies",
-        "business & industrial",
-        "health & beauty",
-        "music",
-        "books",
-        "movies & tv",
-        "video games & consoles",
-        "cameras & photo",
-        "computers/tablets & networking",
-        "cell phones & accessories",
-        "jewelry & watches",
-        "crafts",
-        "pet supplies",
-        "baby essentials",
+        'collectibles & art' => [
+            'collectibles',
+            'antiques',
+            'sports memorabilia',
+            'fine art',
+            'art prints',
+            'comics',
+            'stamps',
+        ],
+        'home & garden' => [
+            'yard, garden & outdoor',
+            'crafts',
+            'home improvement',
+            'pet supplies',
+            'furniture',
+            'kitchen & dining',
+            'bedding'
+        ],
+        'sporting goods' => [
+            'outdoor sports',
+            'team sports',
+            'exercise & fitness',
+            'golf',
+            'water sports',
+            'winter sports',
+            'indoor games'
+        ],
+        'electronics' => [
+            'computers & laptops',
+            'tablets',
+            'cameras & photo',
+            'tv, audio & surveillance',
+            'cell phones & accessories',
+            'smart home devices',
+        ],
+        'auto parts & accessories' => [
+            'gps & security devices',
+            'radar & laser detectors',
+            'care & detailing',
+            'scooter parts & accessories',
+            'car audio & electronics',
+            'motorcycle parts & accessories',
+            'truck parts & accessories'
+        ],
+        'toys & hobbies' => [
+            'radio control',
+            'kids toys',
+            'action figures',
+            'dolls & bears',
+            'model building',
+            'puzzles',
+            'board games'
+        ],
+        'fashion' => [
+            'women',
+            'men',
+            'jewelry & watches',
+            'shoes',
+            'handbags & accessories',
+            'sunglasses & eyewear',
+            'vintage clothing'
+        ],
+        'musical instruments & gear' => [
+            'guitar',
+            'pro audio equipment',
+            'string',
+            'stage lighting & effects',
+            'keyboards & pianos',
+            'drums & percussion',
+            'brass & woodwind'
+        ],
+        'video games & consoles' => [
+            'gaming consoles',
+            'games',
+            'accessories',
+            'virtual reality'
+        ],
+        'health & beauty' => [
+            'personal care',
+            'makeup',
+            'skincare',
+            'hair care',
+            'fragrances',
+            'health care devices'
+        ],
+        'baby' => [
+            'baby gear',
+            'nursery',
+            'feeding',
+            'diapering',
+            'toys',
+            'clothing'
+        ],
     ],
 ];
