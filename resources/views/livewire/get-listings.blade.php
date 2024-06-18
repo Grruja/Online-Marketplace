375ae7ee6cc46bba4e6ce0ddf5018f01024d51a6
AleexxG <alexanderg.sln@gmail.com>
2024-06-18 15:44:51 +0200

    Get listing component added

---
+++ resources/views/livewire/get-listings.blade.php
@@ -0,0 +1,19 @@
+<div class="py-12">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <form class="p-6 text-gray-900 flex gap-2 sm:flex-row flex-col items-center">
+                <div class="sm:w-fit w-full">
+                    <x-select-category-input>Select by category</x-select-category-input>
+                </div>
+                <div class="w-full">
+                    <x-search-input />
+                </div>
+            </form>
+        </div>
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-10">
+            <div class="p-6 text-gray-900">
+                <h1>listings</h1>
+            </div>
+        </div>
+    </div>
+</div>
