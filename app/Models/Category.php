551ad64ee799a4689e0f40698f202c21d264ae66
AleexxG <alexanderg.sln@gmail.com>
2024-06-19 18:24:12 +0200

    Removed subcategory column

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     protected $table = 'categories';
 
-    protected $fillable = ['name', 'subcategory'];
+    protected $fillable = ['name'];
+
+    public function subcategory(): HasMany
+    {
+        return $this->hasMany(Subcategory::class, 'category_id', 'id');
+    }
 }
