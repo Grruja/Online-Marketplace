7e04bc61d7d1b68e740219d2200f61dadda7a8e9
AleexxG <alexanderg.sln@gmail.com>
2024-06-19 18:57:09 +0200

    Listing relation added

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subcategory extends Model
 {
@@ -15,5 +16,10 @@ class Subcategory extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function listing(): HasMany
+    {
+        return $this->hasMany(Listing::class, 'subcategory_id', 'id');
     }
 }
